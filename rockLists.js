81ef6926b7f85c58fdae2ec2f68a675430c4f0f3
lizzb <lizz@dhcp-10-101-77-178.wireless.northwestern.private>
2014-04-19 20:51:39 -0500

    lots metamorphic stuff, images and name from book

---
+++ rockLists.js
@@ -0,0 +1,311 @@
+var sedimentaryRocks = [
+{
+  "sampleNum": 1,
+  "sedimentaryName": "Limestone",
+  "texture": "Chemical",
+  "grainSize": "no grain size",
+  "acidReaction": true,
+  "hardness": 3,
+  "composition": ["calcite"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed01.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup01.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed01.htm"
+},
+{
+  "sampleNum": 2,
+  "sedimentaryName": "Coquina",
+  "texture": "Biologic",
+  "grainSize": "no grain size",
+  "acidReaction": true,
+  "hardness": 3,
+  "composition": ["calcite", "almost entirely shell and skeletal fragments"], 
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed02b.jpg",
+  "img1close": "  http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup02a.jpg",
+  "img1closealt": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup02b.jpg",
+  "img2": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Sed02a.jpg",
+  "img2close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup02c.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed02.htm"
+},
+{
+  "sampleNum": 3,
+  "sedimentaryName": "Rock Gypsum",
+  "texture": "Chemical",
+  "grainSize": "no grain size",
+  "acidReaction": false,
+  "hardness": 2,
+  "composition": ["gypsum"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed03.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed03.htm"
+},
+{
+  "sampleNum": 4,
+  "sedimentaryName": "Fossiliferous Limestone",
+  "texture": "Biologic",
+  "grainSize": "no grain size",
+  "acidReaction": true,
+  "hardness": 3,
+  "composition": ["calcite", "some shell and skeletal fragments"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed04.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup04.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed04.htm"
+},
+{
+  "sampleNum": 5,
+  "sedimentaryName": "Siltstone (Mudstone, Shale)",
+  "texture": "Clastic",
+  "grainSize": "< 1/16",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["quartz", "clay minerals"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed05.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup05.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed05.htm"
+},
+{
+  "sampleNum": 6,
+  "sedimentaryName": "Arkose",
+  "texture": "Clastic",
+  "grainSize": "> 1/16",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["feldspar", "quartz"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed06.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup06.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed06.htm"
+},
+{
+  "sampleNum": 7,
+  "sedimentaryName": "Rock Gypsum",
+  "texture": "Chemical",
+  "grainSize": "no grain size",
+  "acidReaction": false,
+  "hardness": 2,
+  "composition": ["gypsum"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed07.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup07.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed07.htm"
+},
+{
+  "sampleNum": 8,
+  "sedimentaryName": "Conglomerate",
+  "texture": "Clastic",
+  "grainSize": "> 2 mm",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["rounded quartz", "feldspar", "rock fragments"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed08.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup08.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed08.htm"
+},
+{
+  "sampleNum": 9,
+  "sedimentaryName": "Chert",
+  "texture": "Chemical",
+  "grainSize": "no grain size",
+  "acidReaction": false,
+  "hardness": 7,
+  "composition": ["silica (quartz)"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed09.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup09.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed09.htm"
+},
+{
+  "sampleNum": 10,
+  "sedimentaryName": "Sandstone",
+  "texture": "Clastic",
+  "grainSize": "1/16 - 2 mm",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["quartz", "feldspar"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed10.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup10.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed10.htm"
+},
+{
+  "sampleNum": 11,
+  "sedimentaryName": "Sandstone",
+  "texture": "Clastic",
+  "grainSize": "1/16 - 2 mm",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["quartz", "feldspar"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed11.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup11.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed11.htm"
+},
+{
+  "sampleNum": 12,
+  "sedimentaryName": "Bituminous Coal",
+  "texture": "Biologic",
+  "grainSize": "no grain size",
+  "acidReaction": false,
+  "hardness": null,
+  "composition": ["densely compacted organic material and plant fragments"],
+  "img1": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/images/Sed12.jpg",
+  "img1close": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Images/Closeup12.jpg",
+  "url": "http://facweb.bhc.edu/academics/science/harwoodr/geol101/Labs/Sediment/Sed12.htm"
+}
+];
+
+var metamorphicRocks = [ //var data = [
+{
+"foliated": "FOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Flat slaty cleavage is well developed",
+"composition": "Dull luster; breaks into hard flat sheets along the slaty cleavage",
+"metamorphicName": "SLATE",
+"parentRock": "Mudstone or shale",
+"uses":"Roofing slate, table tops, floor tile, and blackboards",
+"img1": "images/Slate.png"
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Phyllite texture well developed more than slaty cleavage",
+"composition": "Breaks along wrinkled or wavy foliation surfaces with shiny metallic luster",
+"metamorphicName": "PHYLLITE",
+"parentRock": "Mudstone, shale, or slate",
+"uses":"Construction stone, decorative stone, sources of gemstones",
+"img1": "images/Phyllite.png"
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Medium- to coarse- grained",
+"texture": "Schistosity: foliation formed by alignment of visible crystals; rock breaks along scaly foliation surfaces; crystalline texture",
+"composition": "Mostly blue or violet needle-like crystals (blue amphibole)",
+"metamorphicName": "Blueschist - SCHIST",
+"parentRock": "Mudstone, shale, slate, or phyllite",
+"uses":"",
+"img1": ""
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Medium- to coarse- grained",
+"texture": "Schistosity: foliation formed by alignment of visible crystals; rock breaks along scaly foliation surfaces; crystalline texture",
+"composition": "Mostly visible sparkling crystals of chlorite +/– actinolite (green amphibole)",
+"metamorphicName": "Greenschist - SCHIST",
+"parentRock": "Mudstone, shale, slate, or phyllite",
+"uses":"",
+"img1": ""
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Medium- to coarse- grained",
+"texture": "Schistosity: foliation formed by alignment of visible crystals; rock breaks along scaly foliation surfaces; crystalline texture",
+"composition": "Mostly visible sparkling crystals of muscovite",
+"metamorphicName": "Muscovite schist - SCHIST",
+"parentRock": "Mudstone, shale, slate, or phyllite",
+"uses":"",
+"img1": "images/MuscoviteSchist.png"
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Medium- to coarse- grained",
+"texture": "Schistosity: foliation formed by alignment of visible crystals; rock breaks along scaly foliation surfaces; crystalline texture",
+"composition": "Mostly visible sparkling crystals of biotite",
+"metamorphicName": "Biotite schist - SCHIST",
+"parentRock": "Mudstone, shale, slate, or phyllite",
+"uses":"",
+"img1": ""
+},
+{
+"foliated": "FOLIATED",
+"grainSize":"Medium- to coarse-grained",
+"texture": "Gneissic banding: minerals segregated into alternating layers gives the rock a banded texture in side view; crystalline texture",
+"composition": "Visible crystals of two or more minerals in alternating light and dark foliated layers",
+"metamorphicName": "GNEISS",
+"parentRock": "Mudstone, shale, slate, phyllite, schist, granite, or diorite",
+"uses":"Construction stone, decorative stone, sources of gemstones",
+"img1": "images/Gneiss.png"
+},
+{
+"foliated": "FOLIATED OR NONFOLIATELD ",
+"grainSize":"Medium- to coarse- grained crystalline texture",
+"texture": "Medium- to coarse- grained crystalline texture",
+"composition": "Mostly visible glossy black amphibole (hornblende) in blade-like crystals",
+"metamorphicName": "AMPHIBIOLITE",
+"parentRock": "Basalt, gabbro, or ultramafic igneous rocks",
+"uses":"Construction stone",
+"img1": ""
+},
+{
+"foliated": "FOLIATED OR NONFOLIATELD ",
+"grainSize":"Crystalline texture",
+"texture": "Crystalline texture",
+"composition": "Green pyroxene + red garnet",
+"metamorphicName": "ECLOGITE",
+"parentRock": "Basalt, gabbro",
+"uses":"Titanium ore",
+"img1": ""
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Glassy texture; slaty cleavage may barely be visible",
+"composition": "Black glossy rock that breaks along uneven or conchoidal fractures (Figure 7.12)",
+"metamorphicName": "ANTHRACITE COAL",
+"parentRock": "Peat, lignite, bituminous coal",
+"uses":"Highest grade coal for clean burning fossil fuel",
+"img1": "images/AnthraciteCoal.png"
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Microcrystalline texture",
+"composition": "Usually a dull dark color; very hard",
+"metamorphicName": "HORNFELS",
+"parentRock": "Any rock type",
+"uses":"Decorative stone",
+"img1": "images/Hornfels.png"
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Microcrystalline texture or no visible grains. May have fibrous asbestos form",
+"composition": "Serpentine; dull or glossy; color usually shades of green",
+"metamorphicName": "SERPENTINITE",
+"parentRock": "Basalt, gabbro, or ultramafic igneous rocks",
+"uses":"Decorative stone",
+"img1": ""
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine-grained or no visible grains",
+"texture": "Microcrystalline texture or no visible grains",
+"composition": "Talc; can be scratched with your fingernail; shades of green, gray, brown, white",
+"metamorphicName": "SOAPSTONE",
+"parentRock": "Basalt, gabbro, or ultramafic igneous rocks",
+"uses":"Art carvings, electrical insulators, talcum powder",
+"img1": ""
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine- to coarse- grained",
+"texture": "Sandy texture",
+"composition": "Quartz sand grains fused together; grains will not rub off like sandstone; usually light colored",
+"metamorphicName": "QUARTZITE",
+"parentRock": "Sandstone",
+"uses":"Construction stone, decorative stone",
+"img1": ""
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine- to coarse- grained",
+"texture": "Microcrystalline (resembling a sugar cube) or medium to coarse crystalline texture",
+"composition": "Calcite (or dolomite) crystals of nearly equal size and tightly fused together; calcite effervesces in dilute HCl; dolomite effervesces only if powdered",
+"metamorphicName": "MARBLE",
+"parentRock": "Limestone",
+"uses":"Art carvings, construction stone, decorative stone, source of lime for agriculture",
+"img1": ""
+},
+{
+"foliated": "NONFOLIATED",
+"grainSize":"Fine- to coarse- grained",
+"texture": "Conglomeratic texture, but breaks across grains ",
+"composition": "Pebbles may be stretched or cut by rock cleavage",
+"metamorphicName": "META-CONGLOMERATE",
+"parentRock": "Conglomerate",
+"uses":"Construction stone, decorative stone",
+"img1": ""
+}
+];
